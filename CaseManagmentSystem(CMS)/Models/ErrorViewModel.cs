bbc1cb80654c759b9e7d2d48a08f7be88aa67e1a
fahad <fahad@FAHAD_PC>
2026-08-18 18:07:56 +0300

    Add project files.

---
+++ CaseManagmentSystem(CMS)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CaseManagmentSystem_CMS_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

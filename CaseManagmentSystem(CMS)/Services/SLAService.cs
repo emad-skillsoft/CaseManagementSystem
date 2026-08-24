3cdd39897028e76b8b514381022fef53c7f67c96
Khaled <khaled@psd.com>
2026-08-24 18:25:01 +0300

    Add-ServicesFolder-ISLAService-ISLAService

---
+++ CaseManagmentSystem(CMS)/Services/SLAService.cs
@@ -0,0 +1,17 @@
+﻿namespace CaseManagementSystem.Services
+{
+    public class SLAService : ISLAService
+    {
+        public DateTime GetDueDate(DateTime slaStartDate, int allowedHours)
+            => throw new NotImplementedException("Implemented in Session 2.");
+
+        public TimeSpan GetTotalDuration(DateTime slaStartDate, DateTime? completedAt, DateTime? asOf = null)
+            => throw new NotImplementedException("Implemented in Session 2.");
+
+        public TimeSpan GetRemainingTime(DateTime dueDate, DateTime? completedAt, DateTime? asOf = null)
+            => throw new NotImplementedException("Implemented in Session 2.");
+
+        public bool IsDelayed(DateTime dueDate, DateTime? completedAt, DateTime? asOf = null)
+            => throw new NotImplementedException("Implemented in Session 3.");
+    }
+}

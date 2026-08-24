472a915a54b091c6b4506012d8547667313dbb76
96653 <96653@BALISTKA>
2026-08-24 18:40:07 +0300

    add WorkflowSeedData

---
+++ CaseManagmentSystem(CMS)/Data/Seed/workflow seed skeleton.cs
@@ -0,0 +1,11 @@
+﻿namespace CaseManagementSystem.Data.Seed
+{
+    public static class WorkflowSeedData
+    {
+        public static Task SeedAsync(IServiceProvider services)
+        {
+            // Full implementation is added in Session 2.
+            return Task.CompletedTask;
+        }
+    }
+}

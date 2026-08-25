8652b577f18e7d018d86bc9752f915bce8cb5a04
pc <mjed.almutairy@gmail.com>
2026-08-25 17:08:41 +0300

    updates

---
+++ CaseManagmentSystem(CMS)/Services/IExcelImportService.cs
@@ -0,0 +1,12 @@
+﻿
+using CaseManagmentSystem_CMS_.Dtos;
+using Microsoft.AspNetCore.Http;
+
+namespace CaseManagementSystem.Services
+{
+    public interface IExcelImportService
+    {
+        Task<(bool IsValid, string ErrorMessage)> ValidateFileAsync(IFormFile file);
+        Task<List<ExcelCaseRowDto>> ReadRowsAsync(IFormFile file);
+    }
+}

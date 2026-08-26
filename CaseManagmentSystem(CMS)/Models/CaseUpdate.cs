3913d16eac02fa30488138482690cb88e77cdf38
96653 <96653@BALISTKA>
2026-08-26 11:52:16 +0300

    implement case updates and Assigned to InProgress transition

---
+++ CaseManagmentSystem(CMS)/Models/CaseUpdate.cs
@@ -0,0 +1,14 @@
+﻿namespace CaseManagementSystem.Models
+{
+    public class CaseUpdate
+    {
+        public int Id { get; set; }
+        public int CaseId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string UpdateText { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+
+        public Case? Case { get; set; }
+        public ApplicationUser? User { get; set; }
+    }
+}
---
+++ CaseManagmentSystem(CMS)/ViewModels/CaseUpdateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CaseManagementSystem.ViewModels
+{
+    public class CaseUpdateViewModel
+    {
+        [Required]
+        public int CaseId { get; set; }
+
+        [Required, MinLength(3)]
+        public string UpdateText { get; set; } = string.Empty;
+    }
+}
+
